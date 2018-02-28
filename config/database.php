7064b05db9ce12da1cf7cbb915895a0d46a6c4af
DonElias <hectorgalindez02@gmail.com>
2018-02-28 17:18:20 -0400

    Fix database 4

--- config/database.php
+++ config/database.php
@@ -39,7 +39,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'mysql_test' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -54,7 +54,7 @@ return [
             'engine' => null,
         ],
 
-        'mysql_test' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
